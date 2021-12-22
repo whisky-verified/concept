e41e6b4ad83f11c5dbd346827e58a8977eb6009d
Thomas Gale <gale.thomasjames@gmail.com>
2021-12-22 12:32:30 +0000

    flexbox center'd

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -3,8 +3,10 @@ import config from "../env/config.json";
 
 export const App = (): React.ReactElement => {
   return (
-    <div className="flex">
-      <h1 className="text-6xl hack-type">{config.NAME}</h1>
+    <div className="flex flex-col items-center">
+      <div className="flex flex-row">
+        <h1 className="flex-shrink text-6xl hack-type">{config.NAME}</h1>
+      </div>
     </div>
   );
 };
