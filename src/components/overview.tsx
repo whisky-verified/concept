77b9c093e6d35b7c8248a4f32baa594791dfd4b5
Thomas Gale <gale.thomasjames@gmail.com>
2021-12-22 19:21:56 +0000

    Placeholder center text.

--- src/components/overview.tsx
+++ src/components/overview.tsx
@@ -1,21 +1,9 @@
 import React from "react";
-import textText from "../env/testText";
 
 export const Overview = (): React.ReactElement => {
   return (
-    <div className="flex flex-grow min-h-0 bg-gray-200 p-2">
-      <div className="flex-1 overflow-y-auto">
-        <h2>Steps</h2>
-        <ul>
-          <li>Deploy to IPFS</li>
-          <li>Demonstrate virtual prototype</li>
-          <li>
-            Build verifiable chain of operations to track whiskey material
-          </li>
-        </ul>
-        <h2>Text Text</h2>
-        <p>{textText}</p>
-      </div>
+    <div className="flex flex-grow bg-gray-200 p-2 items-center justify-center">
+      <h2>Every bottle unique</h2>
     </div>
   );
 };
